8b5ba1149ac7cfe9b33adc5a18261ad56321c79c
AkaraDaimaou <benbeedessy15@gmail.com>
2025-02-02 23:47:04 +0400

    updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -139,7 +139,7 @@ const Contact = () => {
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               />
               {errors.email && <small style={{ color: 'red' }}>{errors.email}</small>}
-            </div>
+            </div> 
             <div>
               <label
                 htmlFor="message"
