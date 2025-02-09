78d8b9fccc4e8ddf70665f092698032abca4fc9f
AkaraDaimaou <benbeedessy15@gmail.com>
2025-02-09 23:08:58 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', // Ensures correct asset paths in deployment
+  build: {
+    outDir: "dist" // Ensures the build goes to 'dist' folder
+  }
 });
